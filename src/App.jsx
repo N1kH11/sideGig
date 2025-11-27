0faf4bc620ec940bc2e26d1ff9cfe933d96e58a5
Nikhil Gautam <n1kh1l@Nikhils-Laptop.local>
2025-11-27 22:10:32 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Bhut busy hai tezz ldki</h1>
+      <h5>Kabhi time nikal kr baat bhi kar lia kar</h5>
+    </>
+  );
+}
+
+export default App;
